ebabf00a8406f20a764ace7d6812ed46aeebb21a
Joseph Callen <jcpowermac@gmail.com>
2017-07-07 10:47:42 -0400

    fixed script

--- che.sh
+++ che.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+
+mkdir -p ${HOME}/che/data
+mkdir -p ${HOME}/che/sync
+mkdir -p ${HOME}/che/unison
+
 docker run -it --rm \
     -v /var/run/docker.sock:/var/run/docker.sock \
-    -v /home/jcallen/che/data:/data \
-    -v /home/jcallen/che/sync:/sync \
-    -v /home/jcallen/che/unison:/unison eclipse/che $@
-    
-
-#    -v /home/jcallen/che/data/instance:/data/instance \
-#    -v /home/jcallen/che/data/backup:/data/backup \
+    -v ${HOME}/che/data:/data \
+    -v ${HOME}/che/sync:/sync \
+    -v ${HOME}/che/unison:/unison eclipse/che $@
